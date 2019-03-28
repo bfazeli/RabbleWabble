c15492adb4810ce4e8770d6cb52621c433cb6215
bfazeli <bfazeli1@gmail.com>
2019-03-27 21:45:09 -0700

    Update: Created a Score originator to utilize in Memento design

--- Rabble Wabble/Models/QuestionGroup.swift
+++ Rabble Wabble/Models/QuestionGroup.swift
@@ -7,6 +7,31 @@
 //
 
 public struct QuestionGroup {
+    
+    // MARK: - Originator
+    public class Score: Codable {
+        public var correctCount: Int = 0
+        public var incorrectCount: Int = 0
+        
+        public init() {}
+    }
+    
     public let questions: [Question]
+    public var score: Score
     public let title: String
+    
+    
+    // For some odd reason score is not being default initi to a blank score
+    // made this initi to initi QuestionGroup w/o having to pass in a Score
+    public init(questions: [Question], title: String) {
+        self.questions = questions
+        self.score = Score()
+        self.title = title
+    }
+    
+    public init(questions: [Question], score: Score, title: String) {
+        self.questions = questions
+        self.score = score
+        self.title = title
+    }
 }
--- Rabble Wabble/Models/QuestionGroupData.swift
+++ Rabble Wabble/Models/QuestionGroupData.swift
@@ -82,6 +82,7 @@ extension QuestionGroup {
       Question(answer: "wo", hint: nil, prompt: "を"),
       Question(answer: "n", hint: nil, prompt: "ん")
     ]
+    
     return QuestionGroup(questions: questions, title: "Hiragana")
   }
 
